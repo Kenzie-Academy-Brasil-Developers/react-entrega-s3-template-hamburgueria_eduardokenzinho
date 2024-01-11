5fc7fa0d41956d02732f6ef95ee61685429b3b7d
Eduardo Fernandes <129226280+eduardokenzinho@users.noreply.github.com>
2024-01-11 19:25:39 -0300

    start of the project

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,13 +1,29 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { CartModal } from "../../components/CartModal";
 import { Header } from "../../components/Header";
 import { ProductList } from "../../components/ProductList";
+import { api } from "../../components/services/api";
 
 export const HomePage = () => {
+   const [allOrders, setOrders] = useState(localOrders ? JSON.parse(localOrders) : []) 
    const [productList, setProductList] = useState([]);
    const [cartList, setCartList] = useState([]);
 
-   // useEffect montagem - carrega os produtos da API e joga em productList
+   const localOrders = localStorage.getItem("@MYREQUESTS")
+
+   useEffect(() => {
+      localStorage.setItem("@MYREQUESTS", JSON.stringify(allOrders))
+   })
+
+   useEffect(() => {
+         const getProducts = async () => {
+            const { data } = await api.get("products");
+            setProductList(data);
+         };
+         getProducts()
+   }, [])
+
+   // feito - useEffect montagem - carrega os produtos da API e joga em productList
    // useEffect atualização - salva os produtos no localStorage (carregar no estado)
    // adição, exclusão, e exclusão geral do carrinho
    // renderizações condições e o estado para exibir ou não o carrinho

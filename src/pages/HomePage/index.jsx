416f307e49b18f55a51869788efdbab57e002819
Eduardo Fernandes <129226280+eduardokenzinho@users.noreply.github.com>
2024-01-12 19:41:37 -0300

    commit

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -5,15 +5,10 @@ import { ProductList } from "../../components/ProductList";
 import { api } from "../../components/services/api";
 
 export const HomePage = () => {
-   const [allOrders, setOrders] = useState(localOrders ? JSON.parse(localOrders) : []) 
+   // const [allOrders, setOrders] = useState(localOrders ? JSON.parse(localOrders) : []) 
+   const [isVisible, setVisible] = useState(false);
    const [productList, setProductList] = useState([]);
    const [cartList, setCartList] = useState([]);
-
-   const localOrders = localStorage.getItem("@MYREQUESTS")
-
-   useEffect(() => {
-      localStorage.setItem("@MYREQUESTS", JSON.stringify(allOrders))
-   })
 
    useEffect(() => {
          const getProducts = async () => {
@@ -23,19 +18,27 @@ export const HomePage = () => {
          getProducts()
    }, [])
 
+   // const localOrders = localStorage.getItem("@MYREQUESTS")
+
+   // useEffect(() => {
+   //    localStorage.setItem("@MYREQUESTS", JSON.stringify(allOrders))
+   // })
+
+
+
    // feito - useEffect montagem - carrega os produtos da API e joga em productList
-   // useEffect atualização - salva os produtos no localStorage (carregar no estado)
+   // feito - useEffect atualização - salva os produtos no localStorage (carregar no estado)
    // adição, exclusão, e exclusão geral do carrinho
-   // renderizações condições e o estado para exibir ou não o carrinho
+   // feito - renderizações condições e o estado para exibir ou não o carrinho
    // filtro de busca
    // estilizar tudo com sass de forma responsiva
 
    return (
       <>
-         <Header />
+         <Header setVisible={setVisible}/>
          <main>
             <ProductList productList={productList} />
-            <CartModal cartList={cartList} />
+            {isVisible ? <CartModal setVisible={setVisible} cartList={cartList} /> : null}
          </main>
       </>
    );
